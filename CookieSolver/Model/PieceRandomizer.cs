9a4054ce46d870f4c93fbae625c5abc3a94ef673
Jerry <4265188+superjerry88@users.noreply.github.com>
2025-02-27 01:56:57 +0800

    Add seed and weight for randomizer

--- CookieSolver/Model/PieceRandomizer.cs
+++ CookieSolver/Model/PieceRandomizer.cs
@@ -1,18 +1,55 @@
 ﻿namespace CookieSolver.Model
 {
 	public class PieceRandomizer
-	{
-		public static readonly int[] PossibleValues = { 1, 2, 3 };
-		public static Random random = new Random();
+    {
+        // To make predictable game for comparing logics
+        public static int Seed { get; set; } = 69420; 
 
-		public static int GetRandomPiece()
-		{
-			return PossibleValues[random.Next(PossibleValues.Length)];
-		}
+        // Weight is to make certain pieces less likely to appear
+        public static readonly WeightedValue[] PossiblePieces =
+        [
+            new(1, 3),
+            new(2, 3), 
+            new(3, 1)
+        ];
 
-		public static Cell GetRandomPosition(List<Cell> possiblePositions)
+        // Make RNG seeded
+        static Random _random = new Random(Seed);
+
+		public static void SetSeed(int seed)
+        {
+            Seed = seed;
+            _random = new Random(seed);
+        }
+
+        public static int GetRandomPiece()
+        {
+            var totalWeight = PossiblePieces.Sum(piece => piece.Weight);
+            var randomValue = _random.Next(totalWeight);
+
+            var currentWeightSum = 0;
+            foreach (var piece in PossiblePieces)
+            {
+                currentWeightSum += piece.Weight;
+                if (randomValue < currentWeightSum)
+                {
+                    return piece.Value;
+                }
+            }
+
+            // Shouldn't reach here but just in-case
+            throw new Exception("GetRandomPiece() logic may be broken ?");
+        }
+
+        public static Cell GetRandomPosition(List<Cell> possiblePositions)
 		{
-			return possiblePositions[random.Next(possiblePositions.Count)];
+			return possiblePositions[_random.Next(possiblePositions.Count)];
 		}
 	}
+
+    public struct WeightedValue(int value, int weight)
+    {
+        public int Value = value;
+        public int Weight = weight;
+    }
 }
